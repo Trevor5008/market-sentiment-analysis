30572b79129da9ba4e08ff947cce21024e925232
sebastianhonorat-dev <youremail@example.com>
2026-03-11 20:54:33 -0400

    updated audit.py

--- src/msa/utils/datacona/audit.py
+++ src/msa/utils/datacona/audit.py
@@ -1,15 +1,55 @@
 import pandas as pd
 from pathlib import Path
-audit_log = []
+from msa import path_list as pl
+import pandas as pd
 
-def collect(df, step):
-    audit_log.append({
+
+_audit_log = []
+_pre_set = False
+
+
+def pre(df_name:str, df:pd.DataFrame, step:str):
+    # Record the dataframe state before a filtering step.
+    
+    global _pre_set
+
+    _audit_log.append({
+        "previous df": df_name,
         "step": step,
-        "rows": len(df)
+        "rows_before": len(df)
     })
 
+    _pre_set = True 
+
+
+def post(df_name:str, df:pd.DataFrame):
+    # Complete the latest audit record with the row count after filtering.
+
+    global _pre_set
+
+
+    if not _pre_set:
+        raise ValueError("\"post\" function used before \"pre\" function.")
+    
+    if not _audit_log:
+        raise ValueError("\"post\" function used before \"pre\" function.")
+
+    _audit_log[-1]["df"] = [df_name]
+    _audit_log[-1]["rows_after"] = [len(df)]
+
+    if _audit_log.count(_audit_log[-1]) > 1:
+        _audit_log.remove(_audit_log[-1])
+        raise ValueError("Duplicated step")
+    
+    
+    _pre_set = False
+
+
 def export(file_name: str):
-    return pd.DataFrame(audit_log).to_csv(f"{file_name}_row_audit.csv")
+    if not _audit_log:
+        raise ValueError("No filtering steps auditted.")
+    return pd.DataFrame(_audit_log).to_csv(pl.PROJECT_ROOT/"data"/"audits"/f"{file_name}_row_audit.csv", index=False)
 
 def reset():
-    audit_log.clear()
+    _audit_log.clear()
+    print("Audit cleared.")
